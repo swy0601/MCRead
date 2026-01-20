71b7e2dfe680be6b813b450af61b4a7d7328e7b8
Iby0708 <73773148+Iby0708@users.noreply.github.com>
2026-01-20 10:39:52 +0800

    Update base_global.py

--- base_global.py
+++ base_global.py
@@ -2,4 +2,4 @@
 # Configure your Python intepreter path here.
 # In default it is "python" and it will use default python in your environment path.
 # But if you use Anaconda or Miniconda instead of default python, you may need to reconfigure this.
-py_intepreter_path = '/home/user/anaconda3/envs/ccrep/bin/python3.9'
+py_intepreter_path = '/home/user/anaconda3/envs/mc/bin/python3.9'
